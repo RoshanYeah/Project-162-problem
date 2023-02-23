cc57ac5e95669fe6479a458eab7d43ffeff0b3bb
RoshanYeah <98729871+RoshanYeah@users.noreply.github.com>
2023-02-23 21:29:56 +1100

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,47 @@
+AFRAME.registerComponent("bowling", {
+    init:function(){
+        this.createBowlingBalls()
+    },
+    createBowlingBalls: function(){
+        window.addEventListener("keydown", (e)=>{
+            if(e.key === " "){
+                var ball = document.createElement("a-entity")
+                ball.setAttribute("geometry",{
+                    primitive:"sphere",
+                    radius:0.3
+                })
+                ball.setAttribute("material","color","black")
+                var cam = document.querySelector("#camera")
+                var pos = cam.getAttribute("position")
+                ball.setAttribute("position",{x:pos.x,y:pos.y-0.5,z:pos.z})
+
+                var cameras = document.querySelector("#camera").object3D
+                var direction = new THREE.Vector3()
+                cameras.getWorldDirection(direction);
+                ball.setAttribute("velocity",direction.multiplyScalar(-7))
+                var scene = document.querySelector("#scene")
+
+                ball.addEventListener("collide", this.removeBall)
+                scene.appendChild(ball)
+            }
+        })
+    },
+    removeBall:function(e){
+        var element = e.detail.target.el
+        var elementHit = e.detail.body.el
+
+        if(elementHit.id.includes("pin")){
+            var impulse = new CANNON.Vec3(0,1,-15);
+            var worldPoint = new CANNON.Vec3().copy(
+                elementHit.getAttribute("position")
+            );
+
+            elementHit.body.applyForce(impulse, worldPoint);
+
+            element.removeEventListener("collide", this.removeBall);
+
+            var scene = document.querySelector("scene")
+            scene.removeChild(element)
+        }
+    },
+})
